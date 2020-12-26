102f4e060b4cb9c3a5f90a2cbae200462c40eb2f
Jun-HNU <1248815798@qq.com>
2020-12-26 16:30:18 +0800

    add eureka-server7001 and eureka-server7002

---
+++ cloud-provider-payment8001/src/main/java/com/hnu/springcloud/PaymentMain8001.java
@@ -0,0 +1,21 @@
+package com.hnu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableSwagger2
+@Configuration
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
