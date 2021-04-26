730c67861603b64370a59ff2dda2802f2315389b
Jun-HNU <1248815798@qq.com>
2021-04-26 21:11:19 +0800

    add read me

--- UserSSO/src/main/java/shopping/mapper/UserMapper.java
+++ UserSSO/src/main/java/shopping/mapper/UserMapper.java
@@ -4,6 +4,7 @@ package shopping.mapper;
 import shopping.pojo.User;
 import tk.mybatis.mapper.common.Mapper;
 
+@org.apache.ibatis.annotations.Mapper
 public interface UserMapper  extends Mapper<User> {
     User queryUserByName(String username);
     User queryByUserCode (String userCode);
